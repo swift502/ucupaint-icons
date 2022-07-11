7f576828a257c88db60fac96b4d0e3dab17e1743
swift502 <blaha.j502@gmail.com>
2022-07-12 01:21:08 +0200

    cropping

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
+from PIL import Image
 
+# Icon names left to right, top to bottom.
 icons = [
     "background_icon",
     "bake_icon",
@@ -23,8 +25,31 @@ icons = [
     # "vector_channel_icon"
 ]
 
+# Last row contains overlays
 overlays = [
     "collapsed",
     "uncollapsed"
     # "add"
 ]
+
+img = Image.open('./svg/grid.png')
+
+tile = img.width / 10
+
+# Regular
+for i in range(len(icons)):
+    col = i % 10
+    row = i // 10
+
+    left = col * tile
+    top = row * tile
+    right = left + tile
+    bottom = top + tile
+
+    coords = (left, top, right, bottom)
+    crop = img.crop(coords)
+    crop.save(f"./png/{icons[i]}.png")
+
+# Overlays
+
+# Disabled
